83783ed65fb67abf8d2ff5b95db213e27fdb4ecf
Victor Vilar <victor.svilar@gmail.com>
2022-09-28 14:34:34 -0300

    incluido arquivos do spring initializer

---
+++ carrinho-compras-api/src/main/java/br/com/carrinhocompras/CarrinhoComprasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carrinhocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoComprasApiApplication.class, args);
+	}
+
+}
---
+++ carrinho-compras-api/src/test/java/br/com/carrinhocompras/CarrinhoComprasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carrinhocompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrinhoComprasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
