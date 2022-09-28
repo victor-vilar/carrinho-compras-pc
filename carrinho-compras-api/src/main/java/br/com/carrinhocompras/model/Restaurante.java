4cd0e3eb32093f2a5921d6d847eea0aacef88fe7
Victor Vilar <victor.svilar@gmail.com>
2022-09-28 19:17:35 -0300

    implementada a classe Restaurante

--- carrinho-compras-api/src/main/java/br/com/carrinhocompras/model/Restaurante.java
+++ carrinho-compras-api/src/main/java/br/com/carrinhocompras/model/Restaurante.java
@@ -1,4 +1,21 @@
 package br.com.carrinhocompras.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="restaurante")
 public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    @OneToMany(cascade= CascadeType.ALL)
+    private List<Produto> cardapio;
+
+    @Embedded
+    private Endereco endereco;
+
 }
